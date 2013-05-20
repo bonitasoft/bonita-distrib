10b8f468e185792794599aa10454d6a0823f7de0
Laurent Vaills <laurent.vaills@bonitasoft.com>
2013-05-20 14:27:53 +0000

    Updated deploy scripts (user/passwd database)

--- deploy-tomcat-bundle-mysql.sh
+++ deploy-tomcat-bundle-mysql.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/bin/bash -v
 
-deploy_dir="/tmp/BOS-6.0.0-beta-016-SNAPSHOT-Tomcat-6.0.35"
+release="BOS-6.0.0-beta-016-SNAPSHOT-Tomcat-6.0.35"
+deploy_dir="/tmp/${release}"
+
+${deploy_dir}/bin/shutdown.sh
+
 rm -fr ${deploy_dir}
-unzip -d /tmp $(dirname $0)/bundle/tomcat/target/BOS-6.0.0-beta-016-SNAPSHOT-Tomcat-6.0.35.zip
+unzip -d /tmp $(dirname $0)/bundle/tomcat/target/${release}.zip
+sed -i 's/username="bonita"/username="root"/;s/password="bpm"/password="root"/' ${deploy_dir}/conf/Catalina/localhost/bonita.xml
+sed -i 's/user=bonita/user=root/;s/password=bpm/password=root/' ${deploy_dir}/conf/bitronix-resources.properties
 
-cp /home/lvaills/Bonita/mysql-connector-java-5.1.24/mysql-connector-java-5.1.24-bin.jar ${deploy_dir}/lib/
-
-sed -i 's/username="bonita"/username="root"/;s/password="bpm"/password="root"/' /tmp/BOS-6.0.0-beta-016-SNAPSHOT-Tomcat-6.0.35/conf/Catalina/localhost/bonita.xml
-
+cp -v /home/lvaills/Bonita/mysql-connector-java-5.1.24/mysql-connector-java-5.1.24-bin.jar ${deploy_dir}/lib/
 
