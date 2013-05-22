1b0949ca1db49777c7de78dc784a2da62a1a18a6
Laurent Vaills <laurent.vaills@bonitasoft.com>
2013-05-22 13:59:28 +0000

    Commented the MySQL specific section

--- deploy-tomcat-bundle-mysql.sh
+++ deploy-tomcat-bundle-mysql.sh
@@ -7,9 +7,9 @@ ${deploy_dir}/bin/shutdown.sh
 
 rm -fr ${deploy_dir}
 unzip -d /tmp $(dirname $0)/bundle/tomcat/target/${release}.zip
-sed -i 's/username="bonita"/username="root"/;s/password="bpm"/password="root"/' ${deploy_dir}/conf/Catalina/localhost/bonita.xml
-sed -i 's/user=bonita/user=root/;s/password=bpm/password=root/' ${deploy_dir}/conf/bitronix-resources.properties
-sed -i 's/h2/mysql/' ${deploy_dir}/bin/setenv.sh
-
-cp -v /home/lvaills/Bonita/mysql-connector-java-5.1.24/mysql-connector-java-5.1.24-bin.jar ${deploy_dir}/lib/
-
+#sed -i 's/username="bonita"/username="root"/;s/password="bpm"/password="root"/' ${deploy_dir}/conf/Catalina/localhost/bonita.xml
+#sed -i 's/user=bonita/user=root/;s/password=bpm/password=root/' ${deploy_dir}/conf/bitronix-resources.properties
+#sed -i 's/h2/mysql/' ${deploy_dir}/bin/setenv.sh
+#
+#cp -v /home/lvaills/Bonita/mysql-connector-java-5.1.24/mysql-connector-java-5.1.24-bin.jar ${deploy_dir}/lib/
+#
