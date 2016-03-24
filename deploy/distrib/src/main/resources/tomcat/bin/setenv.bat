5069b28b7f19871bb4f1cdf933af1b37c44b6098
Baptiste Mesta <baptiste.mesta@gmail.com>
2016-03-24 15:35:33 +0100

    feat(paas_ready): put setup folder in bundle and deploy
    the setup folder contains all resources to setup the platform and can be
    modified in order to customize the platform configuration

--- deploy/distrib/src/main/resources/tomcat/bin/setenv.bat
+++ deploy/distrib/src/main/resources/tomcat/bin/setenv.bat
@@ -4,6 +4,7 @@ rem Set some JVM system properties required by Bonita BPM
 
 rem Bonita home folder (configuration files, temporary folder...) location
 set BONITA_HOME="-Dbonita.home=%CATALINA_HOME%\bonita"
+set PLATFORM_SETUP="-Dorg.bonitasoft.platform.setup.folder=%CATALINA_HOME%\platform-setup"
 
 rem Define the RDMBS vendor use by Bonita Engine to store data
 set DB_OPTS="-Dsysprop.bonita.db.vendor=h2"
@@ -19,6 +20,6 @@ rem Optionnal JAAS configuration. Usually used when delgating authentication to 
 rem set SECURITY_OPTS="-Djava.security.auth.login.config=%CATALINA_HOME%\conf\jaas-standard.cfg"
 
 rem Pass the JVM system properties to Tomcat JVM using CATALINA_OPTS variable
-set CATALINA_OPTS=%CATALINA_OPTS% %BONITA_HOME% %DB_OPTS% %BDM_DB_OPTS% %BTM_OPTS% -Dfile.encoding=UTF-8 -Xshare:auto -Xms1024m -Xmx1024m -XX:MaxPermSize=256m -XX:+HeapDumpOnOutOfMemoryError
+set CATALINA_OPTS=%CATALINA_OPTS% %BONITA_HOME% %PLATFORM_SETUP% %DB_OPTS% %BDM_DB_OPTS% %BTM_OPTS% -Dfile.encoding=UTF-8 -Xshare:auto -Xms1024m -Xmx1024m -XX:MaxPermSize=256m -XX:+HeapDumpOnOutOfMemoryError
 
 set CATALINA_PID=%CATALINA_BASE%\catalina.pid
