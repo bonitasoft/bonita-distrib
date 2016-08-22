c271275e52cdc9a7fa1c0d1454dbd2b1afcc5a04
Romain Bioteau <romain.bioteau@gmail.com>
2016-08-22 10:08:47 +0200

    tr(Bundle): Use auto_server mode in Tomcat bundle for H2 datasources
    * Remove Tomcat H2 listener
    * Change H2 jdbc url mode to file with AUTO_SERVER
    
    fixes [BS-15734](https://bonitasoft.atlassian.net/browse/BS-15734)

--- deploy/distrib/src/main/resources/tomcat/bin/setenv.bat
+++ deploy/distrib/src/main/resources/tomcat/bin/setenv.bat
@@ -3,6 +3,7 @@
 rem Set some JVM system properties required by Bonita BPM
 
 set PLATFORM_SETUP="-Dorg.bonitasoft.platform.setup.folder=%CATALINA_HOME%\setup"
+rem When changing org.bonitasoft.h2.database.dir location, update %CATALINA_HOME%\conf\bitronix-resources.properties datasources accordingly.
 set H2_DATABASE_DIR="-Dorg.bonitasoft.h2.database.dir=%CATALINA_HOME%\database"
 set INCIDENT_LOG_DIR="-Dorg.bonitasoft.engine.incident.folder=%CATALINA_HOME%\logs"
 
