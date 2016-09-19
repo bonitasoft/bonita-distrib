09012c330e49a315197b1bc60442136300858872
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2016-09-19 05:25:26 -0700

    feat(bundles): Have startup scripts call setup init + setup configure (#64)
    closes [BS-15869](https://bonitasoft.atlassian.net/browse/BS-15869)

--- deploy/distrib/src/main/resources/tomcat/bonita-start.bat
+++ deploy/distrib/src/main/resources/tomcat/bonita-start.bat
@@ -1,4 +1,17 @@
 @echo off
 setlocal
 
-call ./bin/startup.bat
+IF NOT EXIST setup GOTO NOSETUPDIR
+   setup\setup.bat init %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+   if errorlevel 1 (
+       echo ERROR 1 Setting up Bonita BPM platform Community edition
+       exit /b 1
+   )
+   setup\setup.bat configure %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+   if errorlevel 1 (
+       echo ERROR 1 Configuring Tomcat bundle
+       exit /b 1
+   )
+
+:NOSETUPDIR
+call bin\startup.bat
--- deploy/distrib/src/main/resources/wildfly/bonita-start.sh
+++ deploy/distrib/src/main/resources/wildfly/bonita-start.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
 
-cd bin
-./standalone.sh
+testReturnCode() {
+  COD_RET=$1
+  if [ ${COD_RET} -ne 0 ]; then
+    echo "ERROR ${COD_RET} $2"
+    exit ${COD_RET}
+  fi
+}
+
+if [ -d "./setup" ]; then
+
+    ./setup/setup.sh init $@
+    testReturnCode $? "Setting up Bonita BPM platform Community edition"
+
+    ./setup/setup.sh configure $@
+    testReturnCode $? "Configuring Wildfly bundle"
+
+fi
+
+./bin/standalone.sh
