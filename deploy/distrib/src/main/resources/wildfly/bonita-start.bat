76da28e76f12dc999e0917092cc94e65407cd6c0
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2016-09-19 05:25:26 -0700

    feat(bundles): Have startup scripts call setup init + setup configure (#64)
    closes [BS-15869](https://bonitasoft.atlassian.net/browse/BS-15869)

--- deploy/distrib/src/main/resources/wildfly/bonita-start.bat
+++ deploy/distrib/src/main/resources/wildfly/bonita-start.bat
@@ -1,5 +1,17 @@
 @echo off
 setlocal
 
-cd bin
-call ./standalone.bat
+IF NOT EXIST setup GOTO NOSETUPDIR
+   setup\setup.bat init %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+   if errorlevel 1 (
+       echo ERROR 1 Setting up Bonita BPM platform Community edition
+       exit /b 1
+   )
+   setup\setup.bat configure %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+   if errorlevel 1 (
+       echo ERROR 1 Configuring Wildfly bundle
+       exit /b 1
+   )
+
+:NOSETUPDIR
+call bin\standalone.bat
