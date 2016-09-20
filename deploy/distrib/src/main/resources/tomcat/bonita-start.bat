e770705fede31963cbb65e3a8d33a95f682135d4
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2016-09-20 08:22:30 -0700

    fix(windows bundles): fix windows bonita-start.bat scripts (Tomcat + Wildfly) (#65)

--- deploy/distrib/src/main/resources/tomcat/bonita-start.bat
+++ deploy/distrib/src/main/resources/tomcat/bonita-start.bat
@@ -2,12 +2,13 @@
 setlocal
 
 IF NOT EXIST setup GOTO NOSETUPDIR
-   setup\setup.bat init %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+   shift
+   call setup\setup.bat init %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
    if errorlevel 1 (
        echo ERROR 1 Setting up Bonita BPM platform Community edition
        exit /b 1
    )
-   setup\setup.bat configure %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+   call setup\setup.bat configure %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
    if errorlevel 1 (
        echo ERROR 1 Configuring Tomcat bundle
        exit /b 1
