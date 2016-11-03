6b02e0ed6f3849a61a6efb5f870ab89a7c477cd4
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2016-11-03 17:41:29 +0100

    ensure Java 8 for WildFly (#75)
    in WildFly, start-bonita.sh and .bat now check that the environment runs Java 8+ and stops if Java < 8 is detected

--- deploy/distrib/src/main/resources/wildfly/start-bonita.bat
+++ deploy/distrib/src/main/resources/wildfly/start-bonita.bat
@@ -1,18 +1,45 @@
 @echo off
 setlocal
 
+rem Check Java version is 8+
+if "x%JAVA_HOME%" == "x" (
+    set JAVA=java
+) else (
+    if not exist "%JAVA_HOME%" (
+        echo JAVA_HOME "%JAVA_HOME%" path doesn't exist
+        exit /b 1
+    ) else (
+        set "JAVA=%JAVA_HOME%\bin\java"
+    )
+)
+
+for /f "tokens=3" %%g in ('%JAVA% -version 2^>^&1 ^| findstr /i "version"') do (
+    set JAVAVER=%%g
+)
+
+set JAVAVER=%JAVAVER:"=%
+for /f "delims=. tokens=2" %%v in ("%JAVAVER%") do (
+    set VERSION_NUMBER=%%v
+)
+
+if "%VERSION_NUMBER%" LSS "8" (
+    echo Wrong Java version %VERSION_NUMBER%^<8. Please set JAVA_HOME variable to a JDK / JRE 8+
+    exit /b 1
+)
+
+
 IF NOT EXIST setup GOTO NOSETUPDIR
-   shift
-   call setup\setup.bat init %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
-   if errorlevel 1 (
-       echo ERROR 1 Setting up Bonita BPM platform Community edition
-       exit /b 1
-   )
-   call setup\setup.bat configure %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
-   if errorlevel 1 (
-       echo ERROR 1 Configuring WildFly bundle
-       exit /b 1
-   )
+shift
+call setup\setup.bat init %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+if errorlevel 1 (
+    exit /b 1
+)
+call setup\setup.bat configure %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+if errorlevel 1 (
+    exit /b 1
+)
 
 :NOSETUPDIR
 call server\bin\standalone.bat
+
+endlocal
