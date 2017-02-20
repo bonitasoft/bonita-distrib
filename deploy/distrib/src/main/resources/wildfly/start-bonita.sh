c7cf910090d34e68493d143b04eba20744c79ff2
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2017-02-20 11:11:24 +0100

    feat(Java 8): check for Java 8 requirement before launching Tomcat (#90)
    * feat(Java 8): WIP tomcat script checks Java version
    
    * feat(Java 8): check for Java 8 requirement before launching Tomcat
    
    Closes to [BS-16232](https://bonitasoft.atlassian.net/browse/BS-16232)

--- deploy/distrib/src/main/resources/wildfly/start-bonita.sh
+++ deploy/distrib/src/main/resources/wildfly/start-bonita.sh
@@ -10,28 +10,39 @@ testReturnCode() {
 # Setup the JVM
 if [ "x$JAVA" = "x" ]; then
     if [ "x$JAVA_HOME" != "x" ]; then
-        JAVA="$JAVA_HOME/bin/java"
+        JAVA_CMD="$JAVA_HOME/bin/java"
     else
-        JAVA="java"
+        JAVA_CMD="java"
     fi
 fi
+
+export JAVA_CMD
+
 # Check Java version is 8+
-java_version=$("$JAVA" -version 2>&1 | grep -i version | sed 's/.*version ".*\.\(.*\)\..*"/\1/; 1q')
-if [ "$java_version" -lt "8" ]; then
-    echo "Wrong Java version ($java_version) < 8. Please set JAVA or JAVA_HOME variable to a JDK / JRE 8+"
+java_version=$("$JAVA_CMD" -version 2>&1 | grep -i version | sed 's/.*version ".*\.\(.*\)\..*"/\1/; 1q')
+echo "java_version: $java_version"
+if [ "x$java_version" = "x" ]; then
+  echo "No Java command could be found. Please set JRE_HOME or JAVA_HOME variable to a JRE / JDK 1.8+"
+  exit 12
+else
+  if [ "$java_version" -lt "8" ]; then
+    echo "Invalid Java version (1.$java_version) < 1.8. Please set JAVA or JAVA_HOME variable to a JDK / JRE 1.8+"
     exit 18
+  fi
 fi
 
+read unused
+
 if [ -d "./setup" ]; then
-	echo "------------------------------------------------------"
-    echo "Initializing and configuring Bonita BPM WildFly bundle"
-	echo "------------------------------------------------------"
+  echo "------------------------------------------------------"
+  echo "Initializing and configuring Bonita BPM WildFly bundle"
+  echo "------------------------------------------------------"
 
-    ./setup/setup.sh init $@
-    testReturnCode $?
+  ./setup/setup.sh init $@
+  testReturnCode $?
 
-    ./setup/setup.sh configure $@
-    testReturnCode $?
+  ./setup/setup.sh configure $@
+  testReturnCode $?
 
 fi
 
