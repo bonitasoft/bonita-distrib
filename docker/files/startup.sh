df9c7a3ec0d8a1609db37ca1b18ab8de2f4e78aa
Pascal GARCIA <pascal.garcia@bonitasoft.com>
2021-10-19 08:49:06 +0200

    feat(log4j2): Use log4j2 to have live update log level and json export format (#395)
    * add log4J2 libraries and logger configuration file to the distrib ( bundle )
    * make docker **community** and **subscription** works well with log4j2 and have the possibility to hot change log levels
    
    closes [RUNTIME-523](https://bonitasoft.atlassian.net/browse/RUNTIME-523)

--- docker/files/startup.sh
+++ docker/files/startup.sh
@@ -4,7 +4,16 @@ set -eo pipefail
 if [ "$(id -u)" = '0' ]; then
 	chmod -R go-rwx /opt/bonita/
   chown -R bonita:bonita /opt/custom-init.d/
+	chown -R bonita:bonita /bonita/run
+	chown -R bonita:bonita /opt/files
 	exec gosu bonita "$BASH_SOURCE" "$@"
+fi
+
+if [[ ! -f /bonita/run/log4j2-appenders.xml ]]
+then
+	mv /opt/files/log4j2/log4j2*.xml /bonita/run/
+	mv /opt/bonita/server/conf/log4j2-loggers.xml /bonita/run/
+	chmod 666 /bonita/run/log4j2*.xml
 fi
 
 # ensure to apply the proper configuration
@@ -23,5 +32,5 @@ then
 	done
 fi
 # launch tomcat
-export LOGGING_CONFIG="-Djava.util.logging.config.file=${BONITA_SERVER_LOGGING_FILE:-/opt/bonita/server/conf/logging.properties}"
+
 exec /opt/bonita/server/bin/catalina.sh run
