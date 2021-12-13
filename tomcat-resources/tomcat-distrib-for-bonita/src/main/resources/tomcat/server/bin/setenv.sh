e39ed458c5d751e33a6c694ef4b400433c9b595b
Pascal GARCIA <pascal.garcia@bonitasoft.com>
2021-12-13 17:33:43 +0100

    fix(log4j2): Enable formatMsgNoLookups to mitigate CVE-2021-44228 (#430)
    *  Enable `formatMsgNoLookups` to mitigate [CVE-2021-44228](https://github.com/advisories/GHSA-jfh8-c2jp-5v3q) with custom client's code

--- tomcat-resources/tomcat-distrib-for-bonita/src/main/resources/tomcat/server/bin/setenv.sh
+++ tomcat-resources/tomcat-distrib-for-bonita/src/main/resources/tomcat/server/bin/setenv.sh
@@ -7,6 +7,9 @@
 
 # Set some JVM system properties required by Bonita
 
+LOGGING_MANAGER="-Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager"
+LOG_CONF_FILE_PATH="-Dlog4j.configurationFile=${CATALINA_BASE}/conf/log4j2-appenders.xml,${CATALINA_BASE}/conf/log4j2-loggers.xml"
+
 PLATFORM_SETUP="-Dorg.bonitasoft.platform.setup.folder=${CATALINA_HOME}/../setup"
 H2_DATABASE_DIR="-Dorg.bonitasoft.h2.database.dir=${CATALINA_HOME}/../h2_database"
 INCIDENT_LOG_DIR="-Dorg.bonitasoft.engine.incident.folder=${CATALINA_HOME}/logs"
@@ -24,18 +27,21 @@ ARJUNA_OPTS="-Dcom.arjuna.ats.arjuna.common.propertiesFile=${CATALINA_HOME}/conf
 # Optional JAAS configuration. Usually used when delegating authentication to LDAP / Active Directory server
 #SECURITY_OPTS="-Djava.security.auth.login.config=${CATALINA_HOME}/conf/jaas-standard.cfg"
 
-# Optional Java debug option. Use to connect an external debugger.
-#DEBUG_OPTS="-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000"
+# Optional JMX remote access Configuration. Used to enable remote JMX agent in tomcat to monitor Heap Memory, Threads, CPU Usage, Classes, and configure various MBeans.
+if [ "$JMX_REMOTE_ACCESS" = 'true' ]; then
+        JMX_REMOTE_ACCESS_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9000 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=true -Dcom.sun.management.jmxremote.password.file=${CATALINA_HOME}/conf/jmxremote.password -Dcom.sun.management.jmxremote.access.file=${CATALINA_HOME}/conf/jmxremote.access"
+fi
 
 # Pass the JVM system properties to Tomcat JVM using CATALINA_OPTS variable
-CATALINA_OPTS="${CATALINA_OPTS} ${PLATFORM_SETUP} ${H2_DATABASE_DIR} ${DB_OPTS} ${BDM_DB_OPTS} ${ARJUNA_OPTS} ${INCIDENT_LOG_DIR} -Dfile.encoding=UTF-8 -Xshare:auto -Xms1024m -Xmx1024m -XX:+HeapDumpOnOutOfMemoryError -Djava.security.egd=file:/dev/./urandom"
+CATALINA_OPTS="${CATALINA_OPTS} ${LOG_CONF_FILE_PATH} ${PLATFORM_SETUP} ${H2_DATABASE_DIR} ${DB_OPTS} ${BDM_DB_OPTS} ${ARJUNA_OPTS} ${INCIDENT_LOG_DIR} ${JMX_REMOTE_ACCESS_OPTS} -Dfile.encoding=UTF-8 -Xshare:auto -Xms1024m -Xmx1024m -XX:+HeapDumpOnOutOfMemoryError -Djava.security.egd=file:/dev/./urandom -Dlog4j2.formatMsgNoLookups=true"
 export CATALINA_OPTS
 
-# Only set CATALINA_PID if not already set (check for empty value) by startup script (usually done by /etc/init.d/tomcat7 but not by startup.sh nor catalina.sh)
+# Only set CATALINA_PID if not already set (check for empty value) by startup script (usually done by /etc/init.d/tomcat8 but not by startup.sh nor catalina.sh)
 if [ -z ${CATALINA_PID+x} ]; then
         CATALINA_PID=${CATALINA_BASE}/catalina.pid;
         export CATALINA_PID;
 fi
 
-# extra lib required at Tomcat startup (for instance bonita juli extensions)
+# extra lib required at Tomcat startup
 export CLASSPATH="${CATALINA_HOME}/lib/ext/*"
+
