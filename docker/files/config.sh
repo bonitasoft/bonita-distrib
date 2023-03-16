a1d3cb0b47eee7b1a6dabb847ca6318f4b1380ac
Pascal GARCIA <pascal.garcia@bonitasoft.com>
2023-03-16 10:38:53 +0100

    feat(docker): install provided page as demand (#560)
    * add new environment properties env properties INSTALL_PROVIDED_PAGES to install provided pages even if a custom app is detected
    
    Relates to https://github.com/bonitasoft/bonita-engine-sp/pull/2514

--- docker/files/config.sh
+++ docker/files/config.sh
@@ -91,7 +91,8 @@ find ${BONITA_PATH}/setup/platform_conf/initial -name "*.properties" | xargs -n1
     -e 's/^platform.tenant.default.username\s*=.*/'"platform.tenant.default.username=${TENANT_LOGIN}"'/' \
     -e 's/^platform.tenant.default.password\s*=.*/'"platform.tenant.default.password=${TENANT_PASSWORD}"'/' \
     -e 's/^#platformAdminUsername\s*=.*/'"platformAdminUsername=${PLATFORM_LOGIN}"'/' \
-    -e 's/^#platformAdminPassword\s*=.*/'"platformAdminPassword=${PLATFORM_PASSWORD}"'/'
+    -e 's/^#platformAdminPassword\s*=.*/'"platformAdminPassword=${PLATFORM_PASSWORD}"'/' \
+    -e 's/^#bonita.runtime.custom-application.install-provided-pages\s*=.*/'"bonita.runtime.custom-application.install-provided-pages=${INSTALL_PROVIDED_PAGES}"'/'
 
 sed -e 's/{{HTTP_API_USERNAME}}/'"${HTTP_API_USERNAME}"'/' \
       -e 's/{{HTTP_API_PASSWORD}}/'"${HTTP_API_PASSWORD}"'/' \
