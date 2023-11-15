4cdeb1c385b981e7074ce19cc685c08028d7149d
Romain Bioteau <romain.bioteau@bonitasoft.com>
2023-11-15 10:35:40 +0100

    chore(release): 2022.1 Docker image release

--- docker/files/startup.sh
+++ docker/files/startup.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 set -eo pipefail
-# if we are root user, we restrict access to files to the user 'bonita'
-if [ "$(id -u)" = '0' ]; then
-	chmod -R go-rwx /opt/bonita/
-  chown -R bonita:bonita /opt/custom-init.d/
-	chown -R bonita:bonita /opt/files
-	exec su-exec  bonita "$BASH_SOURCE" "$@"
-fi
+# only execute bonita specific customization when the executable is tomcat
+# it allows to not run this script when CMD is overridden
+if [[ "$1" == "/opt/bonita/server/bin/catalina.sh" ]]
+  then
+  # if we are root user, we restrict access to files to the user 'bonita'
+  if [ "$(id -u)" = '0' ]; then
+    chmod -R go-rwx /opt/bonita/
+    chown -R bonita:bonita /opt/custom-init.d/
+    chown -R bonita:bonita /opt/files
+    exec su-exec  bonita "$BASH_SOURCE" "$@"
+  fi
 
-# ensure to apply the proper configuration
-if [ ! -f /opt/bonita/${BONITA_VERSION}-configured ]
-then
-	/opt/files/config.sh \
-      && touch /opt/bonita/${BONITA_VERSION}-configured || exit 1
-fi
-if [ -d /opt/custom-init.d/ ]
-then
-  echo "Custom scripts:"
-  find /opt/custom-init.d -name '*.sh' | sort
-	for f in $(find /opt/custom-init.d -name '*.sh' | sort)
-	do
-		[ -f "$f" ] && echo "Executing custom script $f" && . "$f"
-	done
+  # ensure to apply the proper configuration
+  if [ ! -f /opt/bonita/${BONITA_VERSION}-configured ]
+  then
+    /opt/files/config.sh \
+        && touch /opt/bonita/${BONITA_VERSION}-configured || exit 1
+  fi
+  if [ -d /opt/custom-init.d/ ]
+  then
+    echo "Custom scripts:"
+    find /opt/custom-init.d -name '*.sh' | sort
+    for f in $(find /opt/custom-init.d -name '*.sh' | sort)
+    do
+      [ -f "$f" ] && echo "Executing custom script $f" && . "$f"
+    done
+  fi
 fi
 # launch tomcat
 
