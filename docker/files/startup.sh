4b7e22f1d1e1ce62225816b52121bdba557060ce
Romain Bioteau <romain.bioteau@bonitasoft.com>
2023-12-12 18:11:38 +0100

    feat(docker): update ca certificates (#612)

--- docker/files/startup.sh
+++ docker/files/startup.sh
@@ -1,5 +1,33 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Shebang needs to be `bash`, see https://github.com/adoptium/containers/issues/415 for details
+
 set -eo pipefail
+
+# Opt-in is only activated if the environment variable is set
+if [ -n "$USE_SYSTEM_CA_CERTS" ] && [ "$(id -u)" = '0' ]; then
+
+    # Copy certificates from /certificates to the system truststore, but only if the directory exists and is not empty.
+    # The reason why this is not part of the opt-in is because it leaves open the option to mount certificates at the
+    # system location, for whatever reason.
+    if [ -d /certificates ] && [ "$(ls -A /certificates)" ]; then
+        cp -a /certificates/* /usr/local/share/ca-certificates/
+    fi
+
+    CACERT=$JAVA_HOME/lib/security/cacerts
+
+    # JDK8 puts its JRE in a subdirectory
+    if [ -f "$JAVA_HOME/jre/lib/security/cacerts" ]; then
+        CACERT=$JAVA_HOME/jre/lib/security/cacerts
+    fi
+
+    # OpenJDK images used to create a hook for `update-ca-certificates`. Since we are using an entrypoint anyway, we
+    # might as well just generate the truststore and skip the hooks.
+    update-ca-certificates
+
+    trust extract --overwrite --format=java-cacerts --filter=ca-anchors --purpose=server-auth "$CACERT"
+fi
+
+
 # only execute bonita specific customization when the executable is tomcat
 # it allows to not run this script when CMD is overridden
 if [[ "$1" == "/opt/bonita/server/bin/catalina.sh" ]]
@@ -10,7 +38,7 @@ if [[ "$1" == "/opt/bonita/server/bin/catalina.sh" ]]
     chown -R bonita:bonita /opt/bonita/conf/logs/
     chown -R bonita:bonita /opt/custom-init.d/
     chown -R bonita:bonita /opt/files
-    exec su-exec  bonita "$BASH_SOURCE" "$@"
+    exec su-exec bonita "$BASH_SOURCE" "$@"
   fi
 
   # ensure to apply the proper configuration
